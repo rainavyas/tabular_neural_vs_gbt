0d9994d6f59db25f529ae478bc20b31f75dce180
Vyas Raina <sayvaniar@gmail.com>
2021-09-21 00:18:15 +0100

    false

--- regression/train_ngboost.py
+++ regression/train_ngboost.py
@@ -49,7 +49,7 @@ def main():
     params = {
         'Dist' : Normal,
         'Base' : base,
-        'natural_gradient' : True,
+        'natural_gradient' : False,
         'random_state' : args.seed,
         'minibatch_frac' : 0.5,
         'n_estimators' : 500,
