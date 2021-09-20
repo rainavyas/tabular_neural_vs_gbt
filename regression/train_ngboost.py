a5dd8b13a834524e1c84d0329281a7962f73e920
Vyas Raina <sayvaniar@gmail.com>
2021-09-21 00:00:45 +0100

    base

--- regression/train_ngboost.py
+++ regression/train_ngboost.py
@@ -3,6 +3,8 @@ import os
 import sys
 import pandas as pd
 from ngboost import NGBRegressor
+from ngboost.distns import Normal
+from sklearn.tree import DecisionTreeRegressor
 import pickle
 
 def main():
@@ -12,6 +14,8 @@ def main():
     parser.add_argument('dev_in_path', type=str, help='Path to dev_in data')
     parser.add_argument('save_dir_path', type=str, help='Path to directory to save')
     parser.add_argument('--seed', type=int, default=1, help='Specify the global random seed')
+    parser.add_argument('--depth', type=int, default=3, help='Specify the max depth')
+    parser.add_argument('--lr', type=float, default=3, help='Specify the learning rate')
 
     args = parser.parse_args()
 
@@ -30,22 +34,30 @@ def main():
     y_train = df_train['fact_temperature']
     y_dev_in = df_dev_in['fact_temperature']
 
+    # Define base
+    base = DecisionTreeRegressor(
+        criterion = "friedman_mse",
+        min_samples_split = 2,
+        min_samples_leaf = 1,
+        min_weight_fraction_leaf = 0.0,
+        max_depth = args.depth,
+        splitter = 'best',
+        random_state = args.seed
+    )
 
     # Define parameters
     params = {
-        'Dist' : 'Normal',
-        'max_depth' : 4,
-        'max_features' : None, 
-        'max_leaf_nodes' : None,
-        'min_impurity_decrease' : 0.0,
-        'min_impurity_split' : None,
-        'min_samples_leaf' : 1,
-        'min_samples_split' : 2,
-        'min_weight_fraction' : 0.0,
+        'Dist' : Normal,
+        'Base' : base,
+        'natural_gradient' : True,
         'random_state' : args.seed,
         'minibatch_frac' : 0.5,
         'n_estimators' : 500,
-        'learning_rate' : 0.05
+        'learning_rate' : args.lr,
+        'verbose' : True,
+        'verbose_eval' : 5,
+        'tol' : 1e-4,
+        'col_sample' : 0.75,
     }
     ngb = NGBRegressor(**params)
     ngb.fit(X_train, y_train, X_val=X_dev_in, Y_val=y_dev_in)
